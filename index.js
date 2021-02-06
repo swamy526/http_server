d676a67eb186c6a9779f509b8f2be5f36d7d12d9
D V Siva Satyaswamy Kurakula <dvsivasatyaswamy.kurakula@factset.com>
2021-02-06 19:48:40 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app
+  .get('/', (req, res) => res.send('Hello Swamy!'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
