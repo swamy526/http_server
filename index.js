fb7096165ac9bd66e0cd9288f02e2b8979c6d885
D V Siva Satyaswamy Kurakula <dvsivasatyaswamy.kurakula@factset.com>
2021-02-07 14:11:20 +0530

    added files folder

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 5000
 
 app
   .use(express.static(path.join(__dirname, "public")))
-  .get('/', (req, res) => res.send(`<iframe src="https://docs.google.com/gview?url=/Resume.docx&embedded=true"></iframe>`))
+  .get('/', (req, res) => res.send(`<iframe src="https://github.com/swamy526/http_server/blob/main/public/Resume.docx&embedded=true"></iframe>`))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
