1cc0cbb1650dfb0bf53f0a57bffec82d14300884
D V Siva Satyaswamy Kurakula <dvsivasatyaswamy.kurakula@factset.com>
2021-02-07 17:36:30 +0530

    fix(iframe): adjusted iframe height & width

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app
   .use(express.static(path.join(__dirname, "public")))
-  .get('/', (req, res) => res.send(`<iframe src="https://docs.google.com/gview?url=http://kurakula.tech/Resume.docx&embedded=true" frameborder="0">
+  .get('/', (req, res) => res.send(`<iframe src="https://docs.google.com/gview?url=http://kurakula.tech/Resume.docx&embedded=true" style="width:100%; height:100%;" frameborder="0">
   </iframe>`))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
