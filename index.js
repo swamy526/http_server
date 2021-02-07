139d1011540ef47ccc555241ab0f25ee19f563df
D V Siva Satyaswamy Kurakula <dvsivasatyaswamy.kurakula@factset.com>
2021-02-07 14:05:51 +0530

    added resume as landing page

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
 app
-  .get('/', (req, res) => res.send('Hello Swamy!'))
+  .use(express.static(path.join(__dirname, "public")))
+  .get('/', (req, res) => res.send(`<iframe src="https://docs.google.com/gview?url=/Resume.docx&embedded=true"></iframe>`))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
